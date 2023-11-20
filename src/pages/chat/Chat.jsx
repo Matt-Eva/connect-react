05c889004bd1da539c1f36ee7f1113080c7cfd03
Matt Eva <matteweva@gmail.com>
2023-11-20 14:10:08 -0800

    successfully creating and sending messages

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -2,14 +2,15 @@ import { io } from "socket.io-client"
 import { useParams, useOutletContext } from "react-router-dom"
 import { useEffect, useState } from "react"
 
+
 function Chat() {
   const {user} = useOutletContext()
-  // const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const [messages, setMessages] = useState([])
+  const [input, setInput] = useState("")
+  const [socket, setSocket] = useState({})
 
   const chatId = useParams().id
-
-  console.log(messages)
 
   useEffect(() =>{
     const socket = io(import.meta.env.VITE_BACKEND_URL, {
@@ -18,14 +19,16 @@ function Chat() {
         chatId: chatId
       }
     })
+
+    setSocket(socket)
   
     socket.on("disconnect", () =>{
       console.log("disconnected")
     })
   
     socket.on("load", (arg) =>{
-      console.log(arg)
       setMessages(arg)
+      setLoading(false)
     })
 
     socket.on("joined", (arg) =>{
@@ -33,14 +36,37 @@ function Chat() {
     })
   
     socket.on("connect", () =>{
-      console.log("socket", socket.id)
+      // console.log("socket", socket.id)
+    })
+    
+    socket.on("new-message", message =>{
+      setMessages((messages) =>[...messages, message])
     })
 
     return () =>{
       socket.disconnect()
     }
 
-  }, [])
+  }, [user])
+
+  if (loading){
+    return <h1>Loading</h1>
+  }
+
+  const sendMessage = (e) =>{
+    e.preventDefault()
+    if (input === '') return
+
+    const message = {
+      userId: user.uId,
+      chatId: chatId,
+      text: input
+    }
+
+    socket.emit('message', message)
+
+    setInput('')
+  }
 
   const displayMessages = messages.map(message =>{
     const user = message[0]
@@ -50,12 +76,16 @@ function Chat() {
     return <article key={message[1].uId}>{userSpan} {text}</article>
   })
 
-
-
   return (
-    <div>Chat
-      {displayMessages}
-    </div>
+    <main>Chat
+      <section style={{height: "70vh", overflow: "scroll"}}>
+        {displayMessages}
+      </section>
+      <form onSubmit={sendMessage}>
+        <input type="text" value={input} onChange={(e) => setInput(e.target.value)}  />
+        <input type="submit" value='send'/>
+      </form>
+    </main>
   )
 }
 
