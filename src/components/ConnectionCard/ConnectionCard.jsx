43751ebd933551cd6ab5d1f39c28a657676b1ebd
Matt Eva <matteweva@gmail.com>
2023-11-27 09:34:18 -0800

    successfully created new connection

--- src/components/ConnectionCard/ConnectionCard.jsx
+++ src/components/ConnectionCard/ConnectionCard.jsx
@@ -1,9 +1,11 @@
 
 
-function ConnectionCard({name}) {
+function ConnectionCard({name, uId}) {
   return (
-    <div>{name}</div>
+    <div>{name} {uId}</div>
   )
 }
 
 export default ConnectionCard
+
+// 54a3a1e1-50b7-4c7d-a8a4-4b9e6e914664
--- src/components/NewConnectionCard/NewConnectionCard.jsx
+++ src/components/NewConnectionCard/NewConnectionCard.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react"
 
+function NewConnectionCard({name, uId}) {
+    const [connected, setConnected] = useState(false)
 
-function NewConnectionCard({name}) {
+    const addConnection = async () =>{
+        try {
+            const res = await fetch(import.meta.env.VITE_BACKEND_URL + "/new-connection", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include",
+                body: JSON.stringify({name: name, uId: uId})
+            })
+            if(res.ok){
+                setConnected(true)
+            } else{
+                const error = await res.json()
+                console.log(error)
+            }
+        } catch(e){
+            console.error(e)
+        }
+    }
+
     return (
         <article>
-            {name}
-            <button>connect</button>
+            <p>{name} {uId} </p>
+            {connected ? <span> connected</span>: <button onClick={addConnection}>connect</button>}
         </article>
       )
 }
