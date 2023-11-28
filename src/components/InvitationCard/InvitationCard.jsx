56b2da8a41b78fe895c1d979ccdda35936d8acb0
Matt Eva <matteweva@gmail.com>
2023-11-28 15:29:19 -0800

    added accept invitation functionality to invitation card

--- src/components/InvitationCard/InvitationCard.jsx
+++ src/components/InvitationCard/InvitationCard.jsx
@@ -1,10 +1,30 @@
-
+import { useState } from "react"
 
 function InvitationCard({name, uId}) {
+    const [connected, setConnected] = useState(false)
+
+    const accept = async () => {
+        try {
+            const res = await fetch(import.meta.env.VITE_BACKEND_URL + "/accept-invitation", {
+                method: "POST",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({connectionId: uId})
+            })
+            if (res.ok){
+                setConnected(true)
+            }
+        } catch(e){
+            console.error(e)
+        }
+    }
+
   return (
     <div>
         {name}
-        <button>Accept</button>
+        {connected? <span>Connected</span>: <button onClick={accept}>Accept</button>}
     </div>
   )
 }
