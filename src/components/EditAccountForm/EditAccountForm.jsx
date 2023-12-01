ecbe3f15f474da8d589d0ebe9027885b00084022
Matt Eva <matteweva@gmail.com>
2023-12-01 12:36:42 -0800

    successfully patched basic user info

--- src/components/EditAccountForm/EditAccountForm.jsx
+++ src/components/EditAccountForm/EditAccountForm.jsx
@@ -18,42 +18,62 @@ function EditAccountForm({toggleEdit, firstName, lastName, email, profileImg}) {
     }
     const [passwordState, setPasswordState] = useState (initialPasswordState)
 
-    const updateAccount = async (e) =>{
-        e.preventDefault()
-    }
-    
-    const handleInfoChange = async (e) =>{
+    const handleInfoChange = (e) =>{
         setFormState({
             ...formState,
             [e.target.name]: e.target.value
         })
     }
 
-    const updatePassword = async (e) =>{
+    const updateAccount = async (e) =>{
         e.preventDefault()
+        try {
+            const res = await fetch(import.meta.env.VITE_BACKEND_URL + "/my-account", {
+                method: "PATCH",
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({newInfo: formState})
+            })
+            if (res.ok){
+                const updatedInfo = await res.json()
+                alert("Info successfully updated!")
+                setFormState(updatedInfo)
+                setInitialChangeFormState(updatedInfo)
+                setDisableChangeInfo(true)
+            }
+        } catch (e) {
+            console.error(e)
+        }
     }
-
-    const handlePasswordChange = async (e) =>{
+    
+    const handlePasswordChange = (e) =>{
         setPasswordState({
             ...passwordState,
             [e.target.name]: e.target.value
         })
     }
+    
+    const updatePassword = async (e) =>{
+        e.preventDefault()
+    }
  
     const deleteAccount = async () => {
 
     }
+
   return (
     <div>
         <button onClick={toggleEdit}>Back</button>
         <h2>Update Profile Info</h2>
         <button onClick={() => setDisableChangeInfo(false)}>Change Info</button>
         <form onSubmit={updateAccount} onChange={handleInfoChange} disabled={true}>
-            <label for="firstName">First Name</label>
+            <label htmlFor="firstName">First Name</label>
             <input type="text" name="firstName" value={formState.firstName} disabled={disableChangeInfo}/>
-            <label for="lastName">Last Name</label>
+            <label htmlFor="lastName">Last Name</label>
             <input type="text" name="lastName" value={formState.lastName} disabled={disableChangeInfo} />
-            <label for="email">Email</label>
+            <label htmlFor="email">Email</label>
             <input type="text" name="email" value={formState.email} disabled={disableChangeInfo}/>
             <input type="submit" disabled={disableChangeInfo}/>
         </form>
@@ -64,11 +84,11 @@ function EditAccountForm({toggleEdit, firstName, lastName, email, profileImg}) {
         <h2>Change password</h2>
         <button onClick={() => setDisableChangePassword(false)}>Change Password</button>
         <form onSubmit={updatePassword} onChange={handlePasswordChange}>
-            <label for="currentPassword">Current Password</label>
+            <label htmlFor="currentPassword">Current Password</label>
             <input type="password" name="currentPassword" disabled={disableChangePassword}/>
-            <label for="newPassword">New Password</label>
+            <label htmlFor="newPassword">New Password</label>
             <input type="password" name="newPassword" disabled={disableChangePassword}/>
-            <label for="confirmPassword">Confirm Password</label>
+            <label htmlFor="confirmPassword">Confirm Password</label>
             <input type="password" name="confirmPassword" disabled={disableChangePassword}/>
             <input type="submit" disabled={disableChangePassword}/>
         </form>
